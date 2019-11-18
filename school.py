94239d79d812dcbc1d9de7b3dd8a76382a5e1ca0
Chris <cwoo90@gmail.com>
2019-11-18 01:46:33 -0800

    completed lab

--- school.py
+++ school.py
@@ -1,1 +1,19 @@
 class School:
+  def __init__(self, name=None, roster= {}):
+    self.name = name
+    self.roster = roster
+  
+  def add_student(self, name=None, grade=None):
+    if not grade in self.roster:
+      self.roster[grade] = [name]
+    else:
+      self.roster[grade].append(name)
+      
+  def grade(self, grade=None):
+    return self.roster[grade]
+    
+  def sort_roster(self):
+    for grade in self.roster:
+      self.roster[grade].sort()
+    return self.roster
+
